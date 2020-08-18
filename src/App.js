956c7ce0a1e4dde698cf584415069215468400b9
MucaBordini <mucabordini@gmail.com>
2020-08-18 20:48:40 -0300

    Correção dos dados recebidos pelo axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
 
   const [casos , setCasos] = useState([]);
 
-  async function buscar () {
-    const res = await axios.get(
-      'https://covid19-brazil-api.now.sh/api/report/v1'
-    );
-    setCasos(res.data);
-  }
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(
+        'https://covid19-brazil-api.now.sh/api/report/v1'
+      );
+      setCasos(res.data.data);
+    })();
+  }, []);
 
   return (
     <div className="App">
-      <button onClick={() => buscar()}>Carregar casos covid</button>
       <ul>
         {casos.map((caso, index) => {
-          return <li key={index}>{caso.uf} {caso.cases}</li>;
+          return <li key={index}>Estado: {caso.uf} Casos: {caso.cases}</li>;
         })}
       </ul>
     </div>
